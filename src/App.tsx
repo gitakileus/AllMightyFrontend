b315799d8c7d361bae6ae525e7b9409d1c42b8cd
root <root@DESKTOP-7BEF0RB.localdomain>
2022-06-23 07:16:57 -0400

    complete minting page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Landing from './pages/Landing';
+import Minting from './pages/Minting';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Landing />} />
-        {/* <Route path='/mint' element={<MintingPage />} /> */}
+        <Route path='/ALMTYmint' element={<Minting />} />
       </Routes>
+      <ToastContainer toastClassName={'custom-toast-container'} 
+        autoClose={3000}
+        pauseOnFocusLoss={false}
+        pauseOnHover={false}
+        theme={'colored'}
+      />
     </BrowserRouter>
   );
 }
---
+++ src/hook/index.tsx
@@ -0,0 +1,3 @@
+const useContract = () => {
+};
+export default useContract
